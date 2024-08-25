bfd313b6c383d9fe9349b6aa0fce94e50538c199
Ubuntu <azureuser@deploy.on4yneijqmkeljrfzjh01enxjc.bx.internal.cloudapp.net>
2024-08-25 16:02:29 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -3,72 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-// Route::group(['prefix' => 'auth'], function () {
-// });
-// Route::post('/login', 'AuthController@login');
-// Route::get('/getLoggedUser','AuthController@getLoggedUser')->middleware('auth:api');
-
-
-
-// Route::post('/user', 'UserController@store');
-// Route::get('/getUser','UserController@index');
-// Route::get('/showUser/{user_id}','UserController@show');
-
-
-
-// Route::post('/questionStore','QuestionController@store');
-// Route::get('/getQuestion','QuestionController@getQuestion');
-// Route::post('/vote/{question_id}','QuestionController@vote');
-// Route::get('/show/{question_id}','QuestionController@show');
-
-
-
-
-// Route::post('/commentStore','CommentController@store');
-// Route::get('/getAllComment','CommentController@getAllComment');
-// Route::get('/getComment/{question_id}','CommentController@getComment');
-
-Route::middleware(['cors'])->group(function () {
-    Route::post('/login', 'AuthController@login');
-Route::get('/getLoggedUser','AuthController@getLoggedUser')->middleware('auth:api');
-
-
-
-Route::post('/user', 'UserController@store');
-Route::get('/getUser','UserController@index');
-Route::get('/showUser/{user_id}','UserController@show');
-
-
-
-Route::post('/questionStore','QuestionController@store');
-Route::get('/getQuestion','QuestionController@getQuestion');
-Route::post('/vote/{question_id}','QuestionController@vote');
-Route::get('/show/{question_id}','QuestionController@show');
-
-
-
-
-Route::post('/commentStore','CommentController@store');
-Route::get('/getAllComment','CommentController@getAllComment');
-Route::get('/getComment/{question_id}','CommentController@getComment');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-
-
